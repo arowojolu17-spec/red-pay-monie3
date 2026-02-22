bc4f35dacc8d7e54e5f2c675e954c42238532d29
arowojolu17-spec <arowojolu17@gmail.com>
2026-02-22 18:18:26 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "1042785831";
-  const bankName = "VFD MFB";
-  const accountName = "AROWOJOLU ABIDEMI";
+  const accountNumber = "522948966";
+  const bankName = "MONIEPOINT MFB";
+  const accountName = "FAITH ROTIMI OLUWOLE";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
