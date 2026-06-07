ed880787715a7fdae8430f4860d1a87d4b0f18de
arowojolu17-spec <arowojolu17@gmail.com>
2026-06-07 17:50:42 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "6716906552";
-  const bankName = "MONIEPOINT MFB";
-  const accountName = "ALAILE TOPE ";
+  const accountNumber = "2087673762";
+  const bankName = "KUDA";
+  const accountName = "OLUWOLE OLUWADAMILARE ";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
